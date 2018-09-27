9a937a733a23bf2d58bac6ead95a2f34199d9950
quan <13625611069@163.com>
2018-09-27 10:40:58 +0800

    cmake

---
+++ cmake/cmake_practice/test1/main.cpp
@@ -0,0 +1,13 @@
+﻿// test1.cpp: 定义应用程序的入口点。
+//
+#include <iostream>
+
+#include "test1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake。" << endl;
+	return 0;
+}
--- test/myreactor/myreactor.cpp
+++ test/myreactor/myreactor.cpp
@@ -1,5 +1,6 @@
-
-
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <unistd.h>
 #include "myreactor.h"
 
 #define min(a, b) ((a <= b) ? (a) : (b))
@@ -23,5 +24,32 @@ bool CMyReactor::init(const char *ip, short nport)
 	}
 
 	std::cout << "main thread id = " << std::this_thread::get_id() << std::endl;
+
+	m_acceptthread.reset(new std::thread(CMyReactor::accept_thread_proc, this));
+
+	for (auto &t : m_workerthreads)
+	{
+		t.reset(new std::thread(CMyReactor::worker_thread_proc, this));
+	}
+
+	return true;
+}
+
+bool CMyReactor::uninit()
+{
+	m_bStop = true;
+	m_acceptcond.notify_one();
+	m_workercond.notify_all();
+
+	m_acceptthread->join();
+	for (auto &t : m_workerthreads)
+	{
+		t->join();
+	}
+
+	::epollctl(m_epollfd, EPOLL_CTL_DEL, m_listenfd, NULL);
+
+	::shutdown(m_listenfd, SHUT_RDWR);
+	::close
 }
 
