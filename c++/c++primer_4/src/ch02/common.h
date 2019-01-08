8d512db58288da52bbde619071b19069a6cbf358
quan <13625611069@163.com>
2019-01-08 17:17:28 +0800

    primer 02

---
+++ c++/c++primer_4/src/ch02/common.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Sales_item.h"
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::map;
+using std::string;
+using std::pair;
+using std::vector;
