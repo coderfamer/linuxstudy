8b72131e39039e416d0b03af89ebcd37929214a0
quan <13625611069@163.com>
2019-04-29 22:52:59 +0800

    connect

--- unixnet_unpv/volume1/04/ex4_11.c
+++ unixnet_unpv/volume1/04/ex4_11.c
@@ -1,38 +1,42 @@
-#include "unp.h"
 #include <time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include "unp.h"
 
-int main(int argc, char *argv[])
+int main()
 {
-    int listenfd, connfd;
-    socklen_t len;
-    struct sockaddr_in servaddr,cliaddr;
-    listenfd = Socket(AF_INET, SOCK_STREAM, 0);
-    char buff[MAXLINE];
-    timer_t ticks;
+	int listenfd, connfd;
+	socklen_t len;
+	struct sockaddr_in servaddr, cliaddr;
+	char buff[MAXLINE];
+	time_t ticks;
 
-    bzero(&servaddr, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(13);
+	listenfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    Bind(listenfd, (SA *)&servaddr, sizeof(servaddr));
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(13);
 
-    Listen(listenfd, LISTENQ);
+	bind(listenfd, (const struct sockaddr *)&servaddr, sizeof(servaddr));
 
-    for (;;)
-    {
-        len = sizeof(cliaddr);
-        connfd = Accept(listenfd, (SA *)&cliaddr, &len);
-        printf("connection form %s, port %d\n",
-               Inet_ntop(AF_INET, &cliaddr.sin_addr, buff, sizeof(buff)),
-               ntohs(cliaddr.sin_port));
-        ticks = time(NULL);
-        snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
-        Write(connfd, buff, strlen(buff));
+	for (;;) {
+		len = sizeof(cliaddr);
+		connfd = accept(listenfd, (const struct sockaddr *)&cliaddr, &len);
 
-        Close(connfd);
-    }
+		printf("connection from %s, port %d\n",
+			inet_ntop(AF_INET, &cliaddr.sin_addr, buff, sizeof(buff)),
+			ntohs(cliaddr.sin_port));
+
+		ticks = time(NULL);
+		write(connfd, buff, strlen(buff));
+
+		close(connfd);
+	}
 
 
-    return 0;
+
+	return 0;
 }
