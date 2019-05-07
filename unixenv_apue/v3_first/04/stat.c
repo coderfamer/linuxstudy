df213271a46a19ad254820da079fd2dee0704f8e
chenzq <13625611069@163.com>
2019-05-07 19:16:41 +0800

    apueenv 04

---
+++ unixenv_apue/v3_first/04/stat.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main()
+{
+    struct stat *buf = (struct stat *)malloc(sizeof(struct stat));
+    char pathname[24] = "./test";
+
+    stat(pathname, buf);
+
+    printf("st_dev = %lu\nst_szie = %ld\n", buf->st_dev, buf->st_size);
+
+    return 0;
+}
+
